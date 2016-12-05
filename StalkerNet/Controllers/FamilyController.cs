648504758a10366be9d117a81daa7dcdc2e71bcb
ENWehr <enwehr@gmail.com>
2016-12-04 22:18:15 -0500

    fixxed family controller for creating new family

--- StalkerNet/Controllers/FamilyController.cs
+++ StalkerNet/Controllers/FamilyController.cs
@@ -90,10 +90,10 @@ namespace StalkerNet.Controllers
                 {
                     id = families.Count(),
                     familyname = collection["familyname"],
-                    address1 = collection["middlename"],
-                    city = collection["lastname"],
-                    state = collection["cell"],
-                    zip = collection["relationship"],
+                    address1 = collection["address1"],
+                    city = collection["city"],
+                    state = collection["state"],
+                    zip = collection["zip"],
                     homephone = collection["homephone"]
 
                 };
